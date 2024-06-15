61ab1862a6ba029894af582966a8dc39319b1ab8
atriw <jxl42@proton.me>
2024-06-15 18:31:44 +0800

    Restructure

--- flake.nix
+++ flake.nix
@@ -15,20 +15,24 @@
         devshell = devshell.templates.default;
         jupyenv = jupyenv.templates.default;
         python-script = {
-          path = ./python-script;
+          path = ./scripts/python;
           description = "Python self-contained script template";
         };
         haskell-script = {
-          path = ./haskell-script;
+          path = ./scripts/haskell;
           description = "Haskell self-contained script template";
         };
-        haskell-project = {
-          path = ./haskell-project;
-          description = "Haskell project template with sensible defaults";
-        };
         cl-script = {
-          path = ./cl-script;
+          path = ./scripts/cl;
           description = "Common lisp (sbcl) self-contained script template";
+        };
+        c-script = {
+          path = ./scripts/c;
+          description = "C self-contained script template";
+        };
+        haskell-project = {
+          path = ./projects/haskell;
+          description = "Haskell project template with sensible defaults";
         };
       };
     defaultTemplate = self.templates.trivial;
