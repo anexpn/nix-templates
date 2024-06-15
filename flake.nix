1ce94c10ac6eb96b69cf2a5d3b069733d649aa68
atriw <jxl42@proton.me>
2024-06-15 19:15:26 +0800

    Add dev-templates

--- flake.nix
+++ flake.nix
@@ -1,40 +1,37 @@
 {
   description = "My collection of other flake templates";
-  inputs.nixos-templates.url = "github:NixOS/templates";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.jupyenv.url = "github:tweag/jupyenv";
+  inputs.dev-templates.url = "github:the-nix-way/dev-templates";
   outputs = {
     self,
-    nixos-templates,
     devshell,
     jupyenv,
+    dev-templates,
   }: {
-    templates =
-      nixos-templates.templates
-      // {
-        devshell = devshell.templates.default;
-        jupyenv = jupyenv.templates.default;
-        python-script = {
-          path = ./scripts/python;
-          description = "Python self-contained script template";
-        };
-        haskell-script = {
-          path = ./scripts/haskell;
-          description = "Haskell self-contained script template";
-        };
-        cl-script = {
-          path = ./scripts/cl;
-          description = "Common lisp (sbcl) self-contained script template";
-        };
-        c-script = {
-          path = ./scripts/c;
-          description = "C self-contained script template";
-        };
-        haskell-project = {
-          path = ./projects/haskell;
-          description = "Haskell project template with sensible defaults";
-        };
+    templates = dev-templates.templates // {
+      devshell = devshell.templates.default;
+      jupyenv = jupyenv.templates.default;
+      python-script = {
+        path = ./scripts/python;
+        description = "Python self-contained script template";
       };
-    defaultTemplate = self.templates.trivial;
+      haskell-script = {
+        path = ./scripts/haskell;
+        description = "Haskell self-contained script template";
+      };
+      cl-script = {
+        path = ./scripts/cl;
+        description = "Common lisp (sbcl) self-contained script template";
+      };
+      c-script = {
+        path = ./scripts/c;
+        description = "C self-contained script template";
+      };
+      haskell-project = {
+        path = ./projects/haskell;
+        description = "Haskell project template with sensible defaults";
+      };
+    };
   };
 }
